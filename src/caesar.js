073e6208cd4420a96f9cc8ede6715e419dee3b67
Eric Ruhwedel <97764952+ericruhwedel@users.noreply.github.com>
2022-05-04 13:51:28 -0500

    Add files via upload
    Initial commit of all local files into the Github Repo.

---
+++ src/caesar.js
@@ -0,0 +1,31 @@
+const caesarModule = (function () {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  
+   function caesar(input, shift, encode = true) {
+      if (!shift || shift === 0 || shift< -25 || shift > 25) {
+      return false;
+      }
+    input = input.toLowerCase();
+    const result = [];
+    if (!encode) {
+      shift = -shift;
+   }
+  for (let i=0; i<input.length; i++) {
+      let letter = input.charCodeAt(i);
+      if (letter < 97 || letter > 122) {
+        result.push(String.fromCharCode(letter))
+      } else {
+        letter += shift;
+        if (letter >= 97 && letter <= 122) result.push(String.fromCharCode(letter));
+        if (letter < 97)result.push(String.fromCharCode(123 - ( 97 - letter)));
+        if (letter > 122)result.push(String.fromCharCode(96 + (letter - 122)));
+      } 
+  }
+     return result.join("");
+   }
+                        
+    return { caesar,
+    };
+  })();
+  
+  module.exports = { caesar: caesarModule.caesar };
---
+++ src/substitution.js
@@ -0,0 +1,47 @@
+const substitutionModule = (function () {
+  let theAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  function substitution(input, alphabet, encode = true) {
+      if (!alphabet || alphabet.length !=26) return false;
+      for (let i=0; i< input.length; i++) {
+        if (alphabet.indexOf(input[i]) != alphabet.lastIndexOf(input[i]))
+          return false;
+      }
+    if (encode) return newEncode(input, alphabet);
+      return newDecode (input, alphabet);
+  }
+  
+  function newEncode(input, alphabet) {
+   let newCode = [];
+  input = input.toLowerCase();
+  for (let i=0; i<input.length; i++) {
+    let codeIndex = theAlphabet.indexOf(input[i]);
+    let codeLetter = alphabet[codeIndex];
+    if (codeIndex <0) {
+      newCode.push(input[i]);
+    }
+    newCode.push(codeLetter);
+  }
+  return newCode.join("");
+}
+  
+  function newDecode(input, alphabet) {
+    let newDecode = [];
+    for(let i=0; i<input.length; i++) {
+      let decodeIndex = alphabet.indexOf(input[i]);
+      let decodeLetter = theAlphabet[decodeIndex];
+      if (decodeIndex <0) {
+        newDecode.push(input[i]);
+      }
+      newDecode.push(decodeLetter);
+    }
+    return newDecode.join("");
+  }
+                            
+
+  return {
+    substitution,
+  };
+})();
+
+module.exports = { substitution: substitutionModule.substitution };
